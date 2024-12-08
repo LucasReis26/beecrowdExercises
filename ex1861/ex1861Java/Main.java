4bff888dde6256dc247db89ecd0b7296a80c3e6f
Lucas Teixeira Reis <82238378+LucasReis26@users.noreply.github.com>
2024-12-08 10:17:03 -0300

    Iniciando ex1861

---
+++ ex1861/ex1861Java/Main.java
@@ -0,0 +1,15 @@
+package ex1861Java;
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("HALL OF MURDERERS");
+
+		while(sc.hasNext()){
+
+		}
+	}
+}
